e01340b699bb10ea2d38e51d61f4fdec2a1b3e4e
renanrcs <renan.cavalcante@yahoo.com>
2022-12-02 20:00:25 -0300

    validação CF com numeros repetidos

--- JS/validacao.js
+++ JS/validacao.js
@@ -36,11 +36,16 @@ const mensegensErro = {
     dataNascimento: {
         valueMissing: 'O campo de data de nascimento não pode estar vazio',
         customError: 'Você precisa ter mais que 18 anos'
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio',
+        customError: 'CPF informado não é válido!'
     }
 }
 
 const validadores = {
-    dataNascimento: input => validaDataNascimento(input)
+    dataNascimento: input => validaDataNascimento(input),
+    cpf: input => validaCPF(input)
 }
 
 function mostratMensagemErro(tipoInput, input){
@@ -72,4 +77,40 @@ function maiorQue18(data){
     const dataInformada = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
     console.log(dataInformada);         
     return dataAtual >= dataInformada;
+}
+
+function validaCPF(input){
+    const cpfFprmatado = input.value.replace(/\D/g, '');
+    let mensagem = '';
+
+    if(!checaCPFRepetido(cpfFprmatado)){
+        mensagem = 'CPF informado não é válido!';
+    }
+
+    input.setCustomValidity(mensagem);
+}
+
+function checaCPFRepetido(cpf){
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ];
+
+    let cpfValido = true;
+
+    valoresRepetidos.forEach(valor => {
+        if(valor == cpf){
+            cpfValido = false;
+        }
+    })
+
+    return cpfValido;
 }
